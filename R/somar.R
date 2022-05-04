6783240ab6cd1469db3271dece6efe15a9c0ec8f
Edgar Manoel <edgarmrt@gmail.com>
2022-05-04 17:00:39 -0300

    first commit

---
+++ R/somar.R
@@ -0,0 +1,13 @@
+#' Função para somar
+#'
+#' Esta função server para somar dois números
+#'
+#' @param a Um número
+#' @param b outro número
+#'
+#' @examples
+#' somar(3,4)
+#'@export
+somar <- function(a,b){
+  a+b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,13 @@
+#' Função para subtrair
+#'
+#' Esta função server para subtrair dois números
+#'
+#' @param a Um número
+#' @param b outro número
+#'
+#' @examples
+#' subtrair(3,4)
+#'@export
+subtrair <- function(a,b){
+  a-b
+}
